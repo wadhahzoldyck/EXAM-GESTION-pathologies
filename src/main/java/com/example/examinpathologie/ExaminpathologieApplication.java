44a41390aa3507790585879517fdfc0e3856bd2e
wadhah naggui <wadhah.naggui@esprit.tn>
2023-12-06 21:15:29 +0100

    first commit

---
+++ src/main/java/com/example/examinpathologie/ExaminpathologieApplication.java
@@ -0,0 +1,13 @@
+package com.example.examinpathologie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminpathologieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminpathologieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examinpathologie/ExaminpathologieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examinpathologie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminpathologieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
